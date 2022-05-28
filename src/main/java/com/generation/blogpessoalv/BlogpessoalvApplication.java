06eb896c519dac1e4195c29cbd48b4b654f6ff33
Vitória Rodrigues <vitoriafly1@gmail.com>
2022-05-27 23:34:05 -0300

    Blog Pessoal

---
+++ src/main/java/com/generation/blogpessoalv/BlogpessoalvApplication.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoalv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpessoalvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpessoalvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/blogpessoalv/BlogpessoalvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoalv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpessoalvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
