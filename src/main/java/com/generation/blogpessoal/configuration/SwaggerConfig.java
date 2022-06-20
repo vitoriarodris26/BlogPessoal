92650b416f26aafe28d6e94cc96daa0bae36373f
Vitória Rodrigues <vitoriafly1@gmail.com>
2022-06-20 17:14:08 -0300

    Atualização

--- src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -3,16 +3,16 @@ package com.generation.blogpessoal.configuration;
 
 
 	import org.springdoc.core.customizers.OpenApiCustomiser;
-	import org.springframework.context.annotation.Bean;
-	import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-	import io.swagger.v3.oas.models.ExternalDocumentation;
-	import io.swagger.v3.oas.models.OpenAPI;
-	import io.swagger.v3.oas.models.info.Contact;
-	import io.swagger.v3.oas.models.info.Info;
-	import io.swagger.v3.oas.models.info.License;
-	import io.swagger.v3.oas.models.responses.ApiResponse;
-	import io.swagger.v3.oas.models.responses.ApiResponses;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 
 	@Configuration
 	public class SwaggerConfig {
