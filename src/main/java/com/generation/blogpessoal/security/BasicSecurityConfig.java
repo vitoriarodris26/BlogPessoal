5f74723dbd504f4c4ce1404f61e77a64838042ee
Vitória Rodrigues <vitoriafly1@gmail.com>
2022-07-08 15:02:10 -0300

    Atualizado Security

--- src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -13,40 +13,45 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
+@SuppressWarnings("deprecation")
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
+
 	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
+
 	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		 auth.userDetailsService(userDetailsService);
-
-		 auth.inMemoryAuthentication()
-			.withUser("root")
-			.password(passwordEncoder().encode("root"))
-			.authorities("ROLE_USER");
-}
 	
+
+		auth.userDetailsService(userDetailsService);
+
+		auth.inMemoryAuthentication().withUser("root").password(passwordEncoder().encode("root"))
+				.authorities("ROLE_USER");
+
+	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-}
+	}
 
-	 @Override
-		protected void configure(HttpSecurity http) throws Exception {
-		 
-		 http.authorizeRequests()
-			.antMatchers("/usuarios/logar").permitAll()
-			.antMatchers("/usuarios/cadastrar").permitAll()
-			.antMatchers(HttpMethod.OPTIONS).permitAll()
-			.anyRequest().authenticated()
-			.and().httpBasic()
-			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().cors()
-			.and().csrf().disable();
-			
+	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		
+
+		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll().antMatchers("/usuarios/cadastrar")
+				.permitAll().antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated().and().httpBasic()
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
+				.csrf().disable();
+
 	}
 }
